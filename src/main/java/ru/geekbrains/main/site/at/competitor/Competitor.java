be77017ad3a1e02dc6024fbcca7b8e8785a80af8
sv3tlana742 <lightrays.me@gmail.com>
2020-05-28 00:43:41 +0700

    Lesson 2

--- src/main/java/ru/geekbrains/main/site/at/competitor/Competitor.java
+++
@@ -1,9 +0,0 @@
-package ru.geekbrains.main.site.at.competitor;
-
-import ru.geekbrains.main.site.at.obstacles.Treadmill;
-import ru.geekbrains.main.site.at.obstacles.Wall;
-
-public interface Competitor {
-    boolean run(Treadmill treadmill);
-    boolean jump(Wall wall);
-}
--- src/main/java/ru/geekbrains/main/site/at/obstacles/Obstacle.java
+++
@@ -1,8 +0,0 @@
-package ru.geekbrains.main.site.at.obstacles;
-
-public interface Obstacle {
-    int TYPE_TREADMILL = 1;
-    int TYPE_WALL = 2;
-
-    int getType();
-}
